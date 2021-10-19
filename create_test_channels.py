3449c15c090848353ec8da5bca40bf1678269bc3
Jonathan Chan <jonathan@PrimePro.local>
2021-10-19 04:19:49 -0700

    removed slack_api() and instead reference slack_api_http().

--- create_test_channels.py
+++ create_test_channels.py
@@ -14,30 +14,12 @@ import os
 import requests
 import time
 from dotenv import load_dotenv
+from slack_autoarchive import ChannelReaper
 
 load_dotenv()
-
+cr = ChannelReaper()
 number_channels = 50
 channel_name_prefix = 'an-interesting-channel'
-
-def slack_api(api_endpoint, payload):
-    uri = f'https://slack.com/api/{api_endpoint}'
-    header = {'Authorization': f'Bearer {os.environ.get("BOT_SLACK_TOKEN")}'}
-
-    try:
-        resp = requests.post(uri, headers=header, data=payload)
-    except requests.exceptions.RequestException as e:
-        print('Something unexpected happened...')
-        SystemExit(e)
-
-    if resp.status_code == requests.codes.too_many_requests:
-        timeout = int(resp.headers['retry-after']) + 3
-        print(f'rate-limited: Trying again in {timeout} seconds.')
-        time.sleep(timeout)
-        return slack_api(api_endpoint, payload)
-    else:
-        return resp
-
 
 if __name__ == '__main__':
     if os.environ.get('BOT_SLACK_TOKEN', False) == False:
@@ -50,16 +32,14 @@ if __name__ == '__main__':
         channel_name = f'{channel_name_prefix}-{x}'
         payload = {'name': channel_name}
         print(f'Creating channel: {channel_name}')
-        resp = slack_api('conversations.create', payload)
-        response = resp.json()
+        resp = cr.slack_api_http('conversations.create', payload, 'POST')
 
-        if response['ok']:
-            payload = {'channel': response['channel']['id']}
-            resp_leave = slack_api('conversations.leave', payload)
+        if resp['ok']:
+            payload = {'channel': resp['channel']['id']}
+            resp_leave = cr.slack_api_http('conversations.leave', payload, 'POST')
 
-            if not resp_leave.json()['ok']:
+            if not resp_leave['ok']:
                 print(f'Error removing the bot from #{channel_name}: '\
                       f'{resp_leave.json()["error"]}')
         else:
-            print(response)
-            print(response['error'])
+            print(resp)
